b6cacfd9e19946f20c51cfd52e8433c64f5410bf
jmc0592 <jmcole17@yahoo.com>
2016-03-23 09:45:56 -0500

    Added FIFO for multi processors. Prints simulation correct, just need to get averages calculated.

--- JacobProMaker/simulation.cpp
+++ JacobProMaker/simulation.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <time.h>
 #include <map>
+#include <cmath>
 #include "stdlib.h"
 using namespace std;
 
@@ -34,9 +35,15 @@ public:
 };
 
 map <int, process> processMap;
+int processorInUse[51][5];//if using, call clearProcessors() when your function is done
 
+
+
+void clearProcessors();
+//int checkProcessorsInUse();
 void simulate(int );
 void fifo(int );
+void fifoMulti(int );
 void sjf(int );
 void roundRobin(int );
 
@@ -47,14 +54,39 @@ int main()
 	cout << "Enter number of processes to simulate: ";
 	cin >> i;
 
+	clearProcessors();
+
 	simulate(i);
 	fifo(i);
+	fifoMulti(i);
 	sjf(i);
 	roundRobin(i);
 	
 
 	return 0;
 }
+
+void clearProcessors() 
+{
+	for(int i=1; i <= 50; i++) {
+		for(int j=1; j <= 4; j++){
+			processorInUse[i][j] = 0;
+		}
+	}	
+}
+/*
+int checkProcessorsInUse(int iteration)
+{
+	for(int j=1; j <= 4; j++)
+	{
+		if(processorInUse[iteration][j] == 0) {
+			return j;//return processor number available
+		}
+	}
+
+	return 0;
+}
+*/
 
 void simulate(int numOfProcesses)
 {
@@ -211,7 +243,7 @@ void fifo(int numOfProcesses)
 	for(int i=1; i <= numOfProcesses; i++)
 	{
 		tempP = processMap[i];
-		processMap[i].waitTime = tempP.waitTime = overallTime;
+		processMap[i].waitTime = tempP.waitTime = overallTime - tempP.arrivalTime;
 		overallTime = overallTime + tempP.numOfCycles;
 		totalWait = totalWait + processMap[i].waitTime;
 
@@ -226,6 +258,114 @@ void fifo(int numOfProcesses)
 	cout << "Average wait time = " << totalWait / numOfProcesses << endl;
 	cout << "Total penalty time = " << (numOfProcesses-1) * CS << endl;
 	cout << "------" << endl;
+}
+
+void fifoMulti(int numOfProcesses)
+{
+	process tempP;
+	int totalWait1, totalWait2, totalWait3, totalWait4 = 0;
+	int overallTime1, overallTime2, overallTime3, overallTime4 = 0;
+	int pid = 0;
+	int numRows = (ceil((float)numOfProcesses / 4.0f));
+	int numOfProcessesRanInCPU[5] = {0,0,0,0,0};
+	map <int, process> copyMap = processMap;//make a copy so we can change values without affecting other algorithms
+
+	cout << endl << "---FIFO Algorithm Multiprocessor---" << endl;
+
+	for(int i=1; pid < numOfProcesses; i++)
+	{
+		//processor1
+		processorInUse[i][1] = copyMap[++pid].id;
+		numOfProcessesRanInCPU[1]++;
+		copyMap[pid].waitTime = overallTime1 - copyMap[pid].arrivalTime;
+		overallTime1 = overallTime1 + copyMap[pid].numOfCycles;
+		totalWait1 = totalWait1 + copyMap[pid].waitTime;
+		cout << "| CPU1: "  << "pid= " << processorInUse[i][1];
+		if(pid == numOfProcesses) {
+			cout << "| " << endl;
+		}
+
+		//only add to other processors if not on the last process
+		if(pid != numOfProcesses) {
+
+			overallTime1 = overallTime1 + CS;
+
+			if(i != 1) {
+			//processor2
+			processorInUse[i][2] = copyMap[++pid].id;
+			numOfProcessesRanInCPU[2]++;			
+			copyMap[pid].waitTime = overallTime2 - copyMap[pid].arrivalTime;
+			overallTime2 = overallTime2 + copyMap[pid].numOfCycles;
+			totalWait2 = totalWait2 + copyMap[pid].waitTime;
+			}
+			cout << "| CPU2: "  << "pid= " << processorInUse[i][2];
+
+
+
+			if(processorInUse[i][2] != 0)
+			overallTime2 = overallTime2 + CS;
+
+			if(i != 1) {
+			//processor3
+			processorInUse[i][3] = copyMap[++pid].id;
+			numOfProcessesRanInCPU[3]++;
+			copyMap[pid].waitTime = overallTime3 - copyMap[pid].arrivalTime;
+			overallTime3 = overallTime3 + copyMap[pid].numOfCycles;
+			totalWait3 = totalWait3 + copyMap[pid].waitTime;
+			}
+			cout << "| CPU3: "  << "pid= " << processorInUse[i][3];
+
+
+
+			if(processorInUse[i][3] != 0)
+			overallTime3 = overallTime3 + CS;
+
+			if(i != 1) {
+			//processor4
+			processorInUse[i][4] = copyMap[++pid].id;
+			numOfProcessesRanInCPU[4]++;
+			copyMap[pid].waitTime = overallTime4 - copyMap[pid].arrivalTime;
+			overallTime4 = overallTime4 + copyMap[pid].numOfCycles;
+			totalWait4 = totalWait4 + copyMap[pid].waitTime;
+			}
+			cout << "| CPU4: "  << "pid= " << processorInUse[i][4] << "| " << endl;
+
+			if(processorInUse[i][4] != 0)
+			overallTime4 = overallTime4 + CS;
+		}
+
+	}
+/*
+	//print process information for each CPU
+	int k = 0;
+	int j;
+	for(int i=1; k <= numOfProcesses+1; i++) {
+		for(j=1; j <= 4; j++) {
+			if(processorInUse[i][j] != 0) {//only increment if a process was serviced
+				cout << "| CPU" << j << ": " << "pid= " << processorInUse[i][j];
+				k++;
+				if(j % 4 == 0)
+					cout << " | " << endl;
+			}
+		}
+	}
+*/
+	cout << "Average wait time for CPU1 = " << totalWait1 / numOfProcessesRanInCPU[1] << endl;
+	cout << "totalWait1 = " << totalWait1 << endl;
+	cout << "Average wait time for CPU2 = " << totalWait2 / numOfProcessesRanInCPU[2] << endl;
+		cout << "totalWait2 = " << totalWait2 << endl;
+
+	cout << "Average wait time for CPU3 = " << totalWait3 / numOfProcessesRanInCPU[3] << endl;
+		cout << "totalWait3 = " << totalWait3<< endl;
+
+	cout << "Average wait time for CPU4 = " << totalWait4 / numOfProcessesRanInCPU[4] << endl;
+		cout << "totalWait4 = " << totalWait4 << endl;
+
+
+	cout << "Total penalty time = " << (numOfProcesses-4) * CS << endl;
+	cout << "------" << endl;
+
+	clearProcessors();
 }
 
 void roundRobin(int i){
